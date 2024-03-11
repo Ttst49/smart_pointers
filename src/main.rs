80d252016a8a8e7f389b245b1881ea74bbde1034
Ttst <thibautstachnick@gmail.com>
2024-03-11 22:32:51 +0100

    using a box with it enum in order to make recursive lists

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,16 @@
+use crate::List::{Cons, Nil};
+
+#[derive(Debug)]
+enum List{
+    Cons(i64,Box<T>),
+    Nil,
+}
+
 
 fn boxLearning(){
     let b = Box::new(5);
-    println!("b={}",b)
+    let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
+    println!("list={:?}",list)
 }
 
 
