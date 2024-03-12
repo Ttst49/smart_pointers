e297973f4af0e299ac8784cc85e4dbd3f359b4bf
Ttst <thibautstachnick@gmail.com>
2024-03-12 09:55:05 +0100

    new struct MyBox with implementation of new method / deref

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,43 @@
 use crate::List::{Cons, Nil};
+use::std::ops::Deref;
+
 
 #[derive(Debug)]
 enum List{
-    Cons(i64,Box<T>),
+    Cons(i64,Box<List>),
     Nil,
 }
 
 
-fn boxLearning(){
+struct MyBox<T>(T);
+impl<T> MyBox<T>{
+    fn new(x:T)->MyBox<T>{
+        MyBox(x)
+    }
+}
+
+impl<T> Deref for MyBox<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+#[allow(unused)]
+fn box_learning(){
     let b = Box::new(5);
     let list = Cons(1, Box::new(Cons(2, Box::new(Cons(3, Box::new(Nil))))));
-    println!("list={:?}",list)
+    println!("list={:?}",list);
+
+
+    let x = 5;
+    let y = Box::new(x);
+    assert_eq!(5,x);
+    assert_eq!(5,*y);
 }
 
 
 fn main() {
-    boxLearning()
+    box_learning()
 }
